f7ae0ee9a528d692c89638496aa8e394b0819148
Richard Rico <109485217+richardrico037@users.noreply.github.com>
2022-07-18 23:01:26 -0700

    Add files via upload

---
+++ Class/CPP Template/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Richard R. Rico
+ * Created on June 23, 2022, 2:00 PM
+ * Purpose: C++ Template - To be used in all future Assignments
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Mathematical/Physics/Conversions, Higher dimensioned arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+
+    //Declare Variables
+
+    //Initialize Variables
+
+    //Map inputs to outputs -> The Process
+
+    //Display Results
+
+    //Exit stage right
+    return 0;
+}
+
---
+++ Class/Hello, World!/main.cpp
@@ -0,0 +1,40 @@
+/* 
+ * File:   main.cpp
+ * Author: Richard R. Rico
+ * Created on June 23, 2022, 2:00 PM
+ * Purpose: C++ Template - To be used in all future Assignments
+ */
+
+//System Libraries
+
+
+//User Libraries
+ #include <iostream>
+//Global Constants
+//Mathematical/Physics/Conversions, Higher dimensioned arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main() {
+    using namespace std;
+    //Initialize the Random Number Seed
+
+    //Declare Variables
+
+    //Initialize Variables
+
+    //Map inputs to outputs -> The Process
+
+    //Display Results
+    cout<<"Hello World"<<endl;
+    //Another way to do the same thing!
+    cout<<"Hello "<<"World"<<endl;
+    //Another way to do the same thing!
+    cout<<"Hello ";
+    cout<<"World";
+    cout<<endl; 
+    //Exit stage right
+    return 0;
+}
+
